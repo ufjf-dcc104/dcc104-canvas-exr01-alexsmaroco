24351480276c1f7c646f43ce6f2885e7ebb81123
Alex Maroco <ice@lab05m34.nrc.ice.ufjf.br>
2017-03-22 20:44:00 -0300

    Adiciona bordas limitantes para o movimento

--- sprite.js
+++ sprite.js
@@ -20,7 +20,7 @@ Sprite.prototype.desenhar = function(ctx) {
 
 Sprite.prototype.mover = function(dt) {
   this.vx = this.vx + this.ax*dt
-  this.vy = this.vy + (this.ay+20)*dt
+  this.vy = this.vy + (this.ay+50)*dt
   this.x = this.x + this.vx*dt
   this.y = this.y + this.vy*dt
 }
