a1e1a27656ff9e094b5c98b5c18a85b1076f37a1
Alex Maroco <alexsmaroco@gmail.com>
2017-03-29 14:37:17 -0300

    Adiciona mais variaveis ao sprite

--- sprite.js
+++ sprite.js
@@ -2,25 +2,30 @@
 function Sprite(cor,x,y) {
   this.x = x
   this.y = y
+  this.comp = 15
+  this.larg = 15
   this.vx = 0
   this.vy = 0
   this.ax = 0
   this.ay = 0
   this.cor = cor
+  this.podeMover = true
 }
 
 
 
 Sprite.prototype.desenhar = function(ctx) {
   ctx.fillStyle = this.cor
-  ctx.fillRect(this.x,this.y,15,15)
+  ctx.fillRect(this.x,this.y,this.comp,this.larg)
   ctx.strokeStyle = "black"
-  ctx.strokeRect(this.x,this.y,15,15)
+  ctx.strokeRect(this.x,this.y,this.comp,this.larg)
 }
 
 Sprite.prototype.mover = function(dt) {
-  this.vx = this.vx + this.ax*dt
-  this.vy = this.vy + (this.ay+50)*dt
-  this.x = this.x + this.vx*dt
-  this.y = this.y + this.vy*dt
+  if(this.podeMover) {
+	this.vx = this.vx + this.ax*dt
+	this.vy = this.vy + (this.ay+50)*dt
+	this.x = this.x + this.vx*dt
+	this.y = this.y + this.vy*dt
+  }
 }
